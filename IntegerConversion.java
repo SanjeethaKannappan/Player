d8ffcb27ca218d6bb1705a704d3ca70cbbfc8d07
SANJEETHA .K <sanjeetha.kcse@ksrce.ac.in>
2016-07-23 10:15:19 +0530

    Create IntegerConversion.java

---
+++ IntegerConversion.java
@@ -0,0 +1,13 @@
+public class IntegerConversion {
+
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		    System.out.println("Enter a value");
+		    String  s=scan.next();
+		int    intoInt = Integer.parseInt(s);
+		    System.out.println("This is an integer: "+ intoInt);
+		    scan.close();
+		  }
+
+	}
